1564f326e7c587ac6cfd6e3d998c00733b782351
Usama Khalid <104267549+imukhalid@users.noreply.github.com>
2022-04-25 16:59:35 -0700

    feature_header

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usama K.</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+    <!-- =========== Scroll-to-top button =========== -->
+
+    <!-- =========== Light/Dark theme button =========== -->
+
+    <!-- =========== Header =========== -->
+    <header class="sticky">
+        <div class="nav-bar">
+            <a href="#" class="logo">Usama k.</a>
+            <div class="navigation">
+                <div class="nav-items">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#services">Services</a>
+                    <a href="#portfolio">Portfolio</a>
+                    <a href="#contact">Contact</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- =========== Home Section =========== -->
+    <section class="home flex-center" id="home">
+        <div class="home-container">
+            <div class="media-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+            <div class="info">
+                <h2>Hi, I am Usama K.</h2>
+                <h3>QA Engineer</h3>
+                <p>I will test your stunning websites/apps and make it bug free, Highly experienced in manual and automation testing</p>
+                <a href="#" class="btn">Contact Me <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+            <div class="home-image">
+                <img src="images/main-img.png" alt="main-image">
+            </div>
+        </div>
+        <a href="#about" class="scroll-down">Scroll Down <i class="fas fa-arrow-down"></i></a>
+    </section>
+
+    <!-- =========== About Section =========== -->
+
+
+    <!-- =========== Skills Section =========== -->
+
+    <!-- =========== Services Section =========== -->
+
+    <!-- =========== Portfolio Section =========== -->
+
+    <!-- =========== Get in touch Section =========== -->
+
+    <!-- =========== Our Client Section =========== -->
+
+    <!-- =========== Contact Section =========== -->
+</body>
+</html>
